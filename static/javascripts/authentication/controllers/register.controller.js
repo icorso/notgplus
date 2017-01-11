433b3ef1b4f1530a99e68e603604f3b0f6b38060
Maxim Filippov <icorso@yandex.ru>
2017-01-11 13:37:58 +0300

    intorduce login controller, activate the register controller

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -12,6 +12,20 @@
 
   RegisterController.$inject = ['$location', '$scope', 'Authentication'];
 
+  activate();
+
+  /**
+   * @name activate
+   * @desc Actions to be performed when this controller is instantiated
+   * @memberOf thinkster.authentication.controllers.RegisterController
+   */
+  function activate() {
+    // If the user is authenticated, they should not be here.
+    if (Authentication.isAuthenticated()) {
+      $location.url('/');
+    }
+  }
+
   /**
   * @namespace RegisterController
   */
