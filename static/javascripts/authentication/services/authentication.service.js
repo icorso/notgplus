340cc4139bf73ad03961a5e51edc36a0eb4ab649
Maxim Filippov <icorso@yandex.ru>
2017-01-13 15:09:05 +0300

    implement logout

--- static/javascripts/authentication/services/authentication.service.js
+++ static/javascripts/authentication/services/authentication.service.js
@@ -26,6 +26,7 @@
       getAuthenticatedAccount: getAuthenticatedAccount,
       isAuthenticated: isAuthenticated,
       login: login,
+      logout: logout,
       register: register,
       setAuthenticatedAccount: setAuthenticatedAccount,
       unauthenticate: unauthenticate
@@ -140,6 +141,35 @@
      */
      function unauthenticate() {
        delete $cookies.authenticatedAccount;
+     }
+
+     /**
+     * @name logout
+     * @desc Try to log the user out
+     * @returns {Promise}
+     * @memberOf thinkster.authentication.services.Authentication
+     */
+     function logout() {
+       return $http.post('api/v1/auth/logout/')
+         .then(logoutSuccessFn, logoutErrorFn)
+     }
+
+     /**
+     * @name logoutSuccessFn
+     * @desc Unauthenticate and redirect to index with page reload
+     */
+     function logoutSuccessFn(data, status, headers, config) {
+       Authentication.unauthenticate();
+
+       window.location = '/';
+     }
+
+     /**
+     * @name logoutErrorFn
+     * @desc Log "Epic failure!" to the console
+     */
+     function logoutErrorFn(data, status, headers, config) {
+       console.error('Epic failure!');
      }
   }
 })();
